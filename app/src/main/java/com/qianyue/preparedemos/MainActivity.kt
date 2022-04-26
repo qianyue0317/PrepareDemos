503586dd71e5e728932fbd9e69a2db55599a2761
qianyue <1974765766@qq.com>
2022-04-26 22:19:52 +0800

    Initial commit

---
+++ app/src/main/java/com/qianyue/preparedemos/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.qianyue.preparedemos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
